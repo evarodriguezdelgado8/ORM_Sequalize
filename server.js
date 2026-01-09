1da544ed750ffb9fd5ea72f01873660e591b40b2
Eva.Rodriguez <evarodriguez.24@campuscamara.es>
2026-01-09 13:38:27 +0100

    cambios

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import productoRoutes from "./routes/productosRoutes.js";
+import logRoutes from "./routes/logRoutes.js";
+import clientesRoutes from "./routes/clientesRoutes.js";
+import { sequelize } from "./config/db.js";
+import initModels from "./models/init-models.js";
+const app = express();
+app.use(express.json());
+const models = initModels(sequelize);
+// Rutas
+app.use("/api/productos", productoRoutes);
+app.use("/api/log", logRoutes);
+app.use("/api/clientes", clientesRoutes);
+// Sincronizar base de datos
+(async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log("✅ Tablas sincronizadas.");
+  } catch (error) {
+    console.error("❌ Error al sincronizar las tablas:", error);
+  }
+})();
+const PORT = 3000;
+app.listen(PORT, () => console.log(`🚀 Servidor en http://localhost:${PORT}`));
